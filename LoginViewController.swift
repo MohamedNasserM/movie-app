fa781f94dac6e690e87894ccfb811935abeabbe9
MohamedNasserM <102860832+MohamedNasserM@users.noreply.github.com>
2024-08-16 19:43:05 +0300

    Update LoginViewController.swift

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -7,43 +7,38 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
 
     @IBAction func loginBtnAction(_ sender: UIButton) {
-        // will be called when user finishes entering data and press login
+       
         let userName = userNameTF.text
         let password = passwordTF.text
         let shouldGoToNextScreen = verifyUser(userName: userName, password: password)
         
         if shouldGoToNextScreen {
-            //navigate
+           
             let userDefaults = UserDefaults.standard
             userDefaults.set(true, forKey: "loginState")
             let vc = self.storyboard?.instantiateViewController(withIdentifier: "MoviesListVC") as! ViewController
             
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//            appDelegate.window?.rootViewController = vc
+//            
             self.navigationController?.pushViewController(vc, animated: true)
             
         }else{
-            //print error msg
             print("User name or Password is incorrect")
         }
         
     }
     
     func verifyUser(userName:String?, password:String?) -> Bool{
-        // ahmed as user name
-        // 123456 as password
+       
         if (userName == "ahmed" && password == "123456") {
             return true
         }else{
@@ -51,14 +46,7 @@ class LoginViewController: UIViewController {
         }
         
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
